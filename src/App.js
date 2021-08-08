9e7f4a260212b553691c95aec2cc5cde41cf45b4
Cleisson <cleissonconstc@gmail.com>
2021-08-08 18:11:41 -0300

    finishing the page navbar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 import { Posts } from './components/Posts'
+import { Pagination } from './components/Pagination'
 
 function App() {
    const [posts, setPosts] = useState([])
    const [loading, setLoading] = useState(false)
    const [currentPage, setCurrentPage] = useState(1)
-   const [postsPerPage, setPostsPerPage] = useState(10)
+   const [postsPerPage] = useState(10)
 
    useEffect(() => {
       const fetchPosts = async () => {
@@ -26,10 +27,14 @@ function App() {
    const indexOfFirstPost = indexOfLastPost - postsPerPage;
    const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost)
 
+   // Change page number
+   const paginate = (pageNumber) => setCurrentPage(pageNumber)
+
    return (
       <div className="container mt-4">
          <h1 className="text-danger text-center mb-3">My Posts</h1>
          <Posts loading={loading} posts={currentPosts} />
+         <Pagination paginate={paginate} totalPosts={posts.length} postsPerPage={postsPerPage} />
       </div>
    );
 }
